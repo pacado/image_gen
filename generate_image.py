0a7f20927c7313cab0746ec4e8638e6c62f85dbf
Lawrence <ljohny996@gmail.com>
2024-02-13 11:22:27 +0100

    first commit

---
+++ generate_image.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import openai
+import json
+import requests
+from datetime import datetime
+import re
+
+
+def generate_image(OPENAI_API_KEY, query, n=1, size="1024x1024"):
+
+    url = "https://api.openai.com/v1/images/generations"
+    headers = {'Content-Type': 'application/json',
+               'Authorization': "Bearer " + OPENAI_API_KEY}
+    data = json.dumps({"prompt": query,
+                       "n": n,
+                       "size": size})
+
+    response = requests.request("POST", url, headers=headers, data=data)
+    return response
+
+
+def generate_filename(OPENAI_API_KEY, query: str) -> str:
+    openai.api_key = OPENAI_API_KEY
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system",
+             "content": """
+                            You are a helpful assistant designed to summarize text to three words ONLY
+                        """},
+            {"role": "user", "content": query}
+        ],
+    )
+    generation_date = datetime.today().strftime("%Y%m%d")
+    summary = response.choices[0].message.content
+    summary = re.sub('[^ a-zA-Z]', "", summary)
+    filename = "img/" + generation_date + \
+        "_" + summary.replace(" ", "_") + ".jpg"
+    return filename
+
+
+st.title("Image Generator")
+
+with st.form("my_form"):
+    OPENAI_API_KEY = st.text_input("OPENAI_API_KEY")
+    query = st.text_input("Query")
+
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        response = generate_image(OPENAI_API_KEY, query, n=1, size="1024x1024")
+        filename = generate_filename(OPENAI_API_KEY, query)
+
+        if response.status_code == 200:
+            img_url = json.loads(response.text)['data'][0]['url']
+            img_data = requests.get(img_url).content
+            with open(filename, "wb") as image:
+                image.write(img_data)
+
+            st.text("Generated")
+            st.image(filename, caption=query)
