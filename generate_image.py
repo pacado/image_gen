a6f638eab4858674a2cd35b6a72e005398c4b77a
Lawrence <ljohny996@gmail.com>
2024-02-13 11:45:48 +0100

    modifying behavior on pc and cloud

--- generate_image.py
+++ generate_image.py
@@ -4,6 +4,11 @@ import json
 import requests
 from datetime import datetime
 import re
+import platform
+
+platform = platform.processor()
+if "Intel64" in platform:
+    onPC = True
 
 
 def generate_image(OPENAI_API_KEY, query, n=1, size="1024x1024"):
@@ -16,7 +21,8 @@ def generate_image(OPENAI_API_KEY, query, n=1, size="1024x1024"):
                        "size": size})
 
     response = requests.request("POST", url, headers=headers, data=data)
-    return response
+    image_url = json.loads(response.text)['data'][0]['url']
+    return response, image_url
 
 
 def generate_filename(OPENAI_API_KEY, query: str) -> str:
@@ -47,14 +53,15 @@ with st.form("my_form"):
 
     submitted = st.form_submit_button("Submit")
     if submitted:
-        response = generate_image(OPENAI_API_KEY, query, n=1, size="1024x1024")
+        response, image_url = generate_image(
+            OPENAI_API_KEY, query, n=1, size="1024x1024")
         filename = generate_filename(OPENAI_API_KEY, query)
 
         if response.status_code == 200:
-            img_url = json.loads(response.text)['data'][0]['url']
-            img_data = requests.get(img_url).content
-            with open(filename, "wb") as image:
-                image.write(img_data)
+            if onPC:
+                img_data = requests.get(image_url).content
+                with open(filename, "wb") as image:
+                    image.write(img_data)
 
             st.text("Generated")
-            st.image(filename, caption=query)
+            st.image(image_url, caption=query)
